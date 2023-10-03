086910ae07237976abf8f3323934e77505bf0cfb
Dufray <Dufray@DESKTOP-L4P8LJB>
2023-10-02 21:16:46 -0500

    Agregar archivos de proyecto.

---
+++ Models/Comic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUD.Models;
+
+public partial class Comic
+{
+    public int Id { get; set; }
+
+    public string? Nombre { get; set; }
+
+    public string? Edision { get; set; }
+}
---
+++ Models/MvccrudContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD.Models;
+
+public partial class MvccrudContext : DbContext
+{
+    public MvccrudContext()
+    {
+    }
+
+    public MvccrudContext(DbContextOptions<MvccrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Comic> Comics { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+
+    }
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Comic>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__comic__3214EC074445C54A");
+
+            entity.ToTable("comic");
+
+            entity.Property(e => e.Edision)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
